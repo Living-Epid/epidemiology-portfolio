aa2a62f7b6c96029c614b4acb56a54f0c98d2b23
Taiwo Adegbola <taiwo.fieldepid@gmail.com>
2026-07-27 13:57:17 +0100

    Reorganize portfolio: replace Biostatistics with Case Studies

---
+++ 06_R_Toolbox/R built-in Epidemiologic packages.R
@@ -0,0 +1,46 @@
+
+
+
+
+install.packages("outbreaks")      #outbreaks
+library(outbreaks)
+data(package = "outbreaks")
+
+
+
+
+install.packages("NHANES")       #U.S. National Health and Nutrition Examination Survey
+library(NHANES)
+
+
+
+
+library(survival)                 #Survival
+data(package="survival")
+
+
+
+
+install.packages("Epi")          #Epi
+library(Epi)
+data(package="Epi")
+
+
+
+install.packages("epitools")   #epitools
+library(epitools)
+data(package = "epitools")
+
+
+
+library(boot)               #boot
+data(package="boot")
+
+
+
+library(MASS)              #MASS
+data(package="MASS")
+
+
+
+data(package="survey")    #survey
